0db6ca9b93e466986ed2c54aa1d74f8017148fa2
Akash Singh <158601860+DirectToAkash@users.noreply.github.com>
2026-06-05 00:00:50 +0530

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -26,6 +26,22 @@ import BlogTeacherPortfolio from "@/pages/blog/portfolio-website-for-teachers-in
 import BlogArchitectPortfolio from "@/pages/blog/portfolio-website-for-architects-india";
 import BlogCAPortfolio from "@/pages/blog/portfolio-website-for-ca-india";
 import BlogDigitalMarketerPortfolio from "@/pages/blog/portfolio-website-for-digital-marketers-india";
+import BlogSoftwareDeveloperPortfolio from "@/pages/blog/portfolio-website-for-software-developers-india";
+import BlogGraphicDesignerPortfolio from "@/pages/blog/portfolio-website-for-graphic-designers-india";
+import BlogPhotographerPortfolio from "@/pages/blog/portfolio-website-for-photographers-india";
+import BlogInteriorDesignerPortfolio from "@/pages/blog/portfolio-website-for-interior-designers-india";
+import BlogUIUXDesignerPortfolio from "@/pages/blog/portfolio-website-for-uiux-designers-india";
+import BlogDataScientistPortfolio from "@/pages/blog/portfolio-website-for-data-scientists-india";
+import BlogContentWriterPortfolio from "@/pages/blog/portfolio-website-for-content-writers-india";
+import BlogMBAPortfolio from "@/pages/blog/portfolio-website-for-mba-graduates-india";
+import BlogCivilEngineerPortfolio from "@/pages/blog/portfolio-website-for-civil-engineers-india";
+import BlogDentistPortfolio from "@/pages/blog/portfolio-website-for-dentists-india";
+import BlogPsychologistPortfolio from "@/pages/blog/portfolio-website-for-psychologists-india";
+import BlogNutritionistPortfolio from "@/pages/blog/portfolio-website-for-nutritionists-india";
+import BlogFashionDesignerPortfolio from "@/pages/blog/portfolio-website-for-fashion-designers-india";
+import BlogFinancialAdvisorPortfolio from "@/pages/blog/portfolio-website-for-financial-advisors-india";
+import BlogHRPortfolio from "@/pages/blog/portfolio-website-for-hr-professionals-india";
+import BlogNursePortfolio from "@/pages/blog/portfolio-website-for-nurses-india";
 import { useState } from "react";
 
 function Router() {
@@ -48,6 +64,22 @@ function Router() {
       <Route path="/blog/portfolio-website-for-architects-india" component={BlogArchitectPortfolio} />
       <Route path="/blog/portfolio-website-for-ca-india" component={BlogCAPortfolio} />
       <Route path="/blog/portfolio-website-for-digital-marketers-india" component={BlogDigitalMarketerPortfolio} />
+      <Route path="/blog/portfolio-website-for-software-developers-india" component={BlogSoftwareDeveloperPortfolio} />
+      <Route path="/blog/portfolio-website-for-graphic-designers-india" component={BlogGraphicDesignerPortfolio} />
+      <Route path="/blog/portfolio-website-for-photographers-india" component={BlogPhotographerPortfolio} />
+      <Route path="/blog/portfolio-website-for-interior-designers-india" component={BlogInteriorDesignerPortfolio} />
+      <Route path="/blog/portfolio-website-for-uiux-designers-india" component={BlogUIUXDesignerPortfolio} />
+      <Route path="/blog/portfolio-website-for-data-scientists-india" component={BlogDataScientistPortfolio} />
+      <Route path="/blog/portfolio-website-for-content-writers-india" component={BlogContentWriterPortfolio} />
+      <Route path="/blog/portfolio-website-for-mba-graduates-india" component={BlogMBAPortfolio} />
+      <Route path="/blog/portfolio-website-for-civil-engineers-india" component={BlogCivilEngineerPortfolio} />
+      <Route path="/blog/portfolio-website-for-dentists-india" component={BlogDentistPortfolio} />
+      <Route path="/blog/portfolio-website-for-psychologists-india" component={BlogPsychologistPortfolio} />
+      <Route path="/blog/portfolio-website-for-nutritionists-india" component={BlogNutritionistPortfolio} />
+      <Route path="/blog/portfolio-website-for-fashion-designers-india" component={BlogFashionDesignerPortfolio} />
+      <Route path="/blog/portfolio-website-for-financial-advisors-india" component={BlogFinancialAdvisorPortfolio} />
+      <Route path="/blog/portfolio-website-for-hr-professionals-india" component={BlogHRPortfolio} />
+      <Route path="/blog/portfolio-website-for-nurses-india" component={BlogNursePortfolio} />
       <Route component={NotFound} />
     </Switch>
   );
